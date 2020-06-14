1c4e9116411c7aab9f680e7a4ad72bc4551069a3
Matt Peek <peekster225@gmail.com>
2020-06-14 13:51:24 +0000

    Changed return statements

--- src/controllers/AuthController.ts
+++ src/controllers/AuthController.ts
@@ -6,16 +6,13 @@ import * as jwt from 'jsonwebtoken'
 import * as bcrypt from "bcryptjs";
 import { User } from "../entity/User";
 import config from "../config/config";
-import { SSL_OP_NO_SESSION_RESUMPTION_ON_RENEGOTIATION } from "constants";
 
 class AuthController {
 
 	static register = async (req: Request, res: Response) => {
         let { username, password } = req.body;
-        if(!username || !password) {
-            res.status(409).send('Username and password required.');
-            return;
-        }
+        if(!username || !password) 
+            return res.status(409).send('Username and password required.');
 
         const userRepository = getRepository(User);
         let user: User;
@@ -29,24 +26,21 @@ class AuthController {
             return;
         }
 
-        if (user) {
-            res.status(409).send("Username Taken.");
-            return;
-        }
+        if (user) 
+            return res.status(409).send("Username Taken.");
 
         user = userRepository.create({ username: username, password: password });
         userRepository.save(user);
-        res.status(201).send('Registered!');
-        return;
+
+        return res.status(201).send('Registered!');
     };
     
 	static login = async (req: Request, res: Response) => {
         let { username, password } = req.body;
 
-        if(!username || !password) {
-            res.status(409).send('Username and password required.')
-            return;
-        }
+        if(!username || !password) 
+            return res.status(409).send('Username and password required.');
+
 
         const userRepository = getRepository(User)
         let user: User;
@@ -59,28 +53,22 @@ class AuthController {
             return;
         }
 
-        if(!user) {
-            res.status(404).send("Username or password is incorrect.")
-            return;
-        }
+        if(!user) 
+            return res.status(404).send("Username or password is incorrect.");
 
         //check if password is correct
         let passwordsMatch: boolean = await bcrypt.compare(password, user.password)
 
-        if(!passwordsMatch) {
-            res.status(404).send('Username or password is incorrect.')
-            return;
-        }
+        if(!passwordsMatch) 
+            return res.status(404).send('Username or password is incorrect.')
 
-        //TODO: add JWT to user
         const token = jwt.sign(
             { userId: user.id, username: user.username },
             config.jwtSecret,
             { expiresIn: '6h' }
         )
 
-        res.status(202).send('Successful Login!\nToken: ' + token);
-        return;
+        return res.status(202).send('Successful Login!\nToken: ' + token);
 	};
 
     static changePassword = async (req: Request, res: Response) => {
@@ -88,10 +76,8 @@ class AuthController {
         let { oldPassword, newPassword } = req.body;
         const id: string = res.locals.jwtPayload.userId;
 
-        if(!oldPassword || !newPassword) {
-            res.status(409).send('Old and new password required.');
-            return;
-        }
+        if(!oldPassword || !newPassword) 
+            return res.status(409).send('Old and new password required.');
 
         const userRepository = getRepository(User);
         let user: User;
@@ -104,21 +90,21 @@ class AuthController {
             return;
         }
 
-        if(!user) return;
+        if(!user) 
+            return res.status(409).send('No User Found.');
         
         let oldPasswordMatches:boolean = await bcrypt.compare(oldPassword, user.password);
 
-        if(!oldPasswordMatches) {
-            res.status(409).send('Old password is incorrect.');
-            return;
-        }
+        if(!oldPasswordMatches) 
+            return res.status(409).send('Old password is incorrect.');
+        
         
         let saltRounds: number = 10;
         let newHashedPassword:string = await bcrypt.hash(newPassword, saltRounds);
         user.password = newHashedPassword;
         await userRepository.save(user);
-        res.status(202).send("Password Changed.");
-        return;
+        
+        return res.status(202).send("Password Changed.");
     };
 
 }
